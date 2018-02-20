610495b10ed5511367c28639a0e93c85bc4574b5
Pavel Bogachev <pavel.bogachev@kaspersky.com>
2018-02-20 17:18:01 +0300

    add one more relu layer

--- project/nn/main.py
+++ project/nn/main.py
@@ -171,6 +171,7 @@ def main():
     if not os.path.exists(model_path):
         model = models.Sequential()
         model.add(layers.Dense(512, activation='relu', input_shape=(tiles_num * states_num,)))
+        model.add(layers.Dense(512, activation='relu'))
         model.add(layers.Dense(tiles_num, activation='softmax'))
 
         model.compile(optimizer='rmsprop',
