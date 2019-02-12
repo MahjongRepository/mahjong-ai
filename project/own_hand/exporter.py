6a78ae9ccecb9a0713a66d379a050624e41d36bf
Nihisil <lisikhin@gmail.com>
2019-02-12 12:49:01 +0800

    Shrink own hand protocol

--- project/own_hand/exporter.py
+++ project/own_hand/exporter.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from base.utils import encode_melds
 
 
 class OwnHandCSVExporter:
@@ -7,16 +6,12 @@ class OwnHandCSVExporter:
     @staticmethod
     def header():
         return [
-            'log_id',
             'tenpai_player_hand',
             'tenpai_player_waiting',
-            'tenpai_player_melds',
         ]
 
     @staticmethod
     def export_player(tenpai_player):
-        table = tenpai_player.table
-
         tenpai_player_waiting = []
         for x in tenpai_player.waiting:
             tenpai_player_waiting.append('{};{}'.format(
@@ -25,10 +20,8 @@ class OwnHandCSVExporter:
             ))
 
         data = [
-            table.log_id,
             ','.join([str(x) for x in tenpai_player.closed_hand]),
             ','.join(tenpai_player_waiting),
-            encode_melds(tenpai_player.melds)
         ]
 
         return data
--- project/own_hand/protocol.py
+++ project/own_hand/protocol.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-import itertools
 
 
 class OwnHandProtocol:
     tiles_unique = 34
     tiles_num = tiles_unique * 4
-    input_size = tiles_num * 2
+    input_size = tiles_num
 
     def __init__(self):
         self.input_data = []
@@ -13,7 +12,6 @@ class OwnHandProtocol:
 
     def parse_new_data(self, raw_data):
         for index, row in raw_data:
-            melds = [0 for x in range(self.tiles_num)]
             tiles = [0 for x in range(self.tiles_num)]
 
             waiting = [0 for x in range(self.tiles_unique)]
@@ -27,33 +25,5 @@ class OwnHandProtocol:
             for i in range(len(player_hand)):
                 tiles[player_hand[i]] = 1
 
-            melds_temp = self.prepare_melds(row['tenpai_player_melds'])
-            for x in melds_temp:
-                meld_tiles = x[0]
-                for tile in meld_tiles:
-                    melds[tile] = 1
-
-            input_cur = list(itertools.chain(
-                tiles,
-                melds
-            ))
-
-            if len(input_cur) != self.input_size:
-                print("Internal error: len(input_cur) should be %d, but is %d" % (self.input_size, len(input_cur)))
-                exit(1)
-
-            self.input_data.append(input_cur)
+            self.input_data.append(tiles)
             self.output_data.append(waiting)
-
-    def prepare_melds(self, data):
-        melds = []
-        melds_temp = [x for x in data.split(',') if x]
-        for x in melds_temp:
-            temp = x.split(';')
-            tiles = [int(x) for x in temp[1].split('/')]
-            melds.append([
-                tiles,
-                # meld type
-                temp[0]
-            ])
-        return melds
