1f1780ad28038977257c6c1098776aef554e16b7
Nihisil <lisikhin@gmail.com>
2019-02-19 16:32:43 +0800

    Move common model code to base.Model

--- project/betaori/model.py
+++ project/betaori/model.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 import logging
-import os
-import pickle
 
-from keras import layers
-from keras import models
-from keras.callbacks import Callback
-from keras.models import load_model
-from keras.utils import HDF5Matrix
-from mahjong.tile import TilesConverter
 import numpy as np
+from keras.callbacks import Callback
+from mahjong.tile import TilesConverter
 
 # from base.plot_utils import plot_history
+from base.model import Model
 from betaori.protocol import BetaoriProtocol
 
 logger = logging.getLogger('logs')
@@ -24,7 +19,7 @@ class LoggingCallback(Callback):
         logger.info(msg)
 
 
-class BetaoriModel:
+class BetaoriModel(Model):
     model_name = 'betaori.h5'
 
     model_attributes = {
@@ -36,111 +31,7 @@ class BetaoriModel:
     units = 1024
     batch_size = 256
 
-    def __init__(self, root_dir, data_path, print_predictions, epochs, need_visualize):
-        self.model_path = os.path.join(root_dir, self.model_name)
-        self.data_path = os.path.join(data_path, 'betaori')
-        self.print_predictions = print_predictions
-        self.need_visualize = need_visualize
-
-        self.epochs = epochs
-
-        self.graphs_data = []
-
-    def remove_model(self):
-        if os.path.exists(self.model_path):
-            os.remove(self.model_path)
-
-    def run(self):
-        logger.info('Epochs: {}'.format(self.epochs))
-        logger.info('Batch size: {}'.format(self.batch_size))
-        logger.info('Model attributes: {}'.format(self.model_attributes))
-        logger.info('Output: {}'.format(self.output))
-        logger.info('Units: {}'.format(self.units))
-        logger.info('')
-
-        test_file_path = os.path.join(self.data_path, 'test.p')
-        test_data = pickle.load(open(test_file_path, 'rb'))
-
-        test_samples = len(test_data.input_data)
-        test_input = np.asarray(test_data.input_data).astype('float32')
-        test_output = np.asarray(test_data.output_data).astype('float32')
-        test_verification = test_data.verification_data
-        logger.info('Test data size = {}'.format(test_samples))
-
-        if not os.path.exists(self.model_path):
-            data_files_temp = os.listdir(self.data_path)
-            data_files = []
-            for f in data_files_temp:
-                if not f.endswith('.h5'):
-                    continue
-
-                data_files.append(f)
-
-            train_files = sorted(data_files)
-            logger.info('{} files will be used for training'.format(len(train_files)))
-
-            model = models.Sequential()
-            model.add(layers.Dense(self.units, activation='relu', input_shape=(BetaoriProtocol.input_size,)))
-            model.add(layers.Dense(self.units, activation='relu'))
-            model.add(layers.Dense(BetaoriProtocol.tiles_unique, activation=self.output))
-
-            model.compile(**self.model_attributes)
-
-            for n_epoch in range(1, self.epochs + 1):
-                logger.info('')
-                logger.info('Processing epoch #{}...'.format(n_epoch))
-
-                for train_file in train_files:
-                    logger.info('Processing {}...'.format(train_file))
-                    h5_file_path = os.path.join(self.data_path, train_file)
-
-                    train_input = HDF5Matrix(h5_file_path, 'input_data')
-                    train_output = HDF5Matrix(h5_file_path, 'output_data')
-
-                    logger.info('Train data size = {}'.format(train_input.size / BetaoriProtocol.input_size))
-
-                    model.fit(
-                        train_input,
-                        train_output,
-                        # we need it to work with md5 data
-                        shuffle='batch',
-                        epochs=1,
-                        batch_size=self.batch_size,
-                        validation_data=(test_input, test_output),
-                        callbacks=[LoggingCallback()]
-                    )
-
-                logger.info('Predictions after epoch #{}'.format(n_epoch))
-                self.calculate_predictions(model,
-                                           test_input,
-                                           test_verification,
-                                           False,
-                                           n_epoch)
-
-                # We save model after each epoch
-                logger.info('Saving model, please don\'t interrupt...')
-                model.save(self.model_path)
-                logger.info('Model saved')
-        else:
-            model = load_model(self.model_path)
-
-        if self.graphs_data:
-            best_result = sorted(self.graphs_data, key=lambda x: x['avg_min_wait_pos'], reverse=True)[0]
-            logger.info('Best result: {}'.format(best_result))
-
-        results = model.evaluate(test_input, test_output, verbose=1)
-        logger.info('results: loss = {}'.format(results))
-
-        logger.info('')
-        logger.info('Final predictions')
-        self.calculate_predictions(model,
-                                   test_input,
-                                   test_verification,
-                                   self.print_predictions,
-                                   None)
-
-        if self.need_visualize:
-            plot_history(self.graphs_data)
+    input_size = BetaoriProtocol.input_size
 
     def tiles_34_to_sting_unsorted(self, tiles):
         string = ''
--- project/own_hand/model.py
+++ project/own_hand/model.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
 import logging
-import os
-import pickle
 
-import numpy as np
-from keras import layers
-from keras import models
-from keras.models import load_model
-from keras.utils import HDF5Matrix
-from mahjong.tile import TilesConverter
-
+from base.model import Model
 from own_hand.protocol import OwnHandProtocol
 
 logger = logging.getLogger('logs')
 
 
-class OwnHandModel:
+class OwnHandModel(Model):
     model_name = 'own_hand.h5'
 
     model_attributes = {
@@ -27,96 +19,7 @@ class OwnHandModel:
     units = 1024
     batch_size = 512
 
-    def __init__(self, root_dir, data_path, print_predictions, epochs, need_visualize):
-        self.model_path = os.path.join(root_dir, self.model_name)
-        self.data_path = os.path.join(data_path, 'own_hand')
-        self.print_predictions = print_predictions
-        self.need_visualize = need_visualize
-
-        self.epochs = epochs
-
-        self.graphs_data = []
-
-    def remove_model(self):
-        if os.path.exists(self.model_path):
-            os.remove(self.model_path)
-
-    def run(self):
-        logger.info('Epochs: {}'.format(self.epochs))
-        logger.info('Batch size: {}'.format(self.batch_size))
-        logger.info('Model attributes: {}'.format(self.model_attributes))
-        logger.info('Output: {}'.format(self.output))
-        logger.info('Units: {}'.format(self.units))
-        logger.info('')
-
-        test_file_path = os.path.join(self.data_path, 'test.p')
-        test_data = pickle.load(open(test_file_path, 'rb'))
-
-        test_samples = len(test_data.input_data)
-        test_input = np.asarray(test_data.input_data).astype('float32')
-        test_output = np.asarray(test_data.output_data).astype('float32')
-        logger.info('Test data size = {}'.format(test_samples))
-
-        if not os.path.exists(self.model_path):
-            data_files_temp = os.listdir(self.data_path)
-            data_files = []
-            for f in data_files_temp:
-                if not f.endswith('.h5'):
-                    continue
-
-                data_files.append(f)
-
-            train_files = sorted(data_files)
-            logger.info('{} files will be used for training'.format(len(train_files)))
-
-            model = models.Sequential()
-            model.add(layers.Dense(self.units, activation='relu', input_shape=(OwnHandProtocol.input_size,)))
-            model.add(layers.Dense(self.units, activation='relu'))
-            model.add(layers.Dense(OwnHandProtocol.tiles_unique, activation=self.output))
-
-            model.compile(**self.model_attributes)
-
-            for n_epoch in range(1, self.epochs + 1):
-                logger.info('')
-                logger.info('Processing epoch #{}...'.format(n_epoch))
-
-                for train_file in train_files:
-                    logger.info('Processing {}...'.format(train_file))
-                    h5_file_path = os.path.join(self.data_path, train_file)
-
-                    train_input = HDF5Matrix(h5_file_path, 'input_data')
-                    train_output = HDF5Matrix(h5_file_path, 'output_data')
-
-                    logger.info('Train data size = {}'.format(train_input.size / OwnHandProtocol.input_size))
-
-                    model.fit(
-                        train_input,
-                        train_output,
-                        # we need it to work with md5 data
-                        shuffle='batch',
-                        epochs=1,
-                        batch_size=self.batch_size,
-                        validation_data=(test_input, test_output)
-                    )
-
-                logger.info('Predictions after epoch #{}'.format(n_epoch))
-                self.calculate_predictions(model,
-                                           test_input,
-                                           test_output,
-                                           n_epoch)
-
-                # We save model after each epoch
-                logger.info('Saving model, please don\'t interrupt...')
-                model.save(self.model_path)
-                logger.info('Model saved')
-        else:
-            model = load_model(self.model_path)
-
-        results = model.evaluate(test_input, test_output, verbose=1)
-        logger.info('results [loss, acc] = {}'.format(results))
-
-        if self.print_predictions:
-            self.calculate_predictions(model, test_input, test_output, None)
+    input_size = OwnHandProtocol.input_size
 
     def calculate_predictions(self, model, test_input, test_output, epoch):
         predictions = model.predict(test_input, verbose=1)
